f0d6d125abcf3b0f92d8d3b51addcefd9cdbc9fc
Serik Kelment <serikklement@gmail.com>
2019-11-22 10:10:11 +0200

    create project

---
+++ DumpHunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DumpHunt
+//
+//  Created by Serik on 22.11.2019.
+//  Copyright © 2019 Serik_Klement. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
